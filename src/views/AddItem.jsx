0b7126cf7bbacac17185043033516e4ce36c30d6
Khadija Gwarzo <khadijagarbag@gmail.com>
2023-04-12 16:39:28 +0100

    create add item UI

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,40 @@
+import './AddItem.css';
+
 export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+	return (
+		<form className="add-item-form">
+			<label htmlFor="item-name">Item name:</label>
+			<input type="text" id="item-name" />
+			<span>How soon will you buy this again</span>
+			<radiogroup>
+				<div>
+					<input
+						type="radio"
+						value={7}
+						name="buying-frequency"
+						id="soon"
+					></input>
+					<label htmlFor="soon">Soon</label>
+				</div>
+				<div>
+					<input
+						type="radio"
+						value={14}
+						name="buying-frequency"
+						id="kind-of-soon"
+					></input>
+					<label htmlFor="kind-of-soon">Kind Of Soon</label>
+				</div>
+				<div>
+					<input
+						type="radio"
+						value={30}
+						name="buying-frequency"
+						id="not-soon"
+					></input>
+					<label htmlFor="not-soon">Not Soon</label>
+				</div>
+			</radiogroup>
+		</form>
+	);
 }
