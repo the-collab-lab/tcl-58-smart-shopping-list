a74db2776ddac953ddd7a572a74e467726593d3a
Khadija Gwarzo <khadijagarbag@gmail.com>
2023-04-18 14:56:40 +0100

    create filter functionality
    Co-authored-by: Peace Sandy <peacesandy04@gmail.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,18 +1,46 @@
 import { ListItem } from '../components';
 import { Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export function List({ data, listToken }) {
+	const [searchValue, setSearchValue] = useState('');
+	const [items, setItems] = useState([]);
+	const [filteredItems, setFilteredItems] = useState([]);
+
+	useEffect(() => {
+		setItems(data);
+	}, [data]);
+
+	useEffect(() => {
+		const filteredItems = items.filter((item) =>
+			item.name.toLowerCase().includes(searchValue),
+		);
+
+		setFilteredItems(filteredItems);
+	}, [items, searchValue]);
+
+	function handleChange(event) {
+		setSearchValue(event.target.value.toLowerCase());
+	}
+
 	if (!listToken) {
 		return <Navigate to="/" />;
 	}
 
 	return (
 		<>
-			<p>
-				Hello from the <code>/list</code> page!
-			</p>
+			<form>
+				<label htmlFor="search-input"> Filter items </label>
+				<input
+					type="search"
+					placeholder="Start typing here..."
+					name="search-item"
+					id="search-input"
+					onChange={handleChange}
+				/>
+			</form>
 			<ul>
-				{data.map((item) => (
+				{filteredItems.map((item) => (
 					<ListItem key={item.id} name={item.name} />
 				))}
 			</ul>
